78415eb94bf953649916f85829e4d2d352dd7f3c
Mikael <mikael.c.mendes@gmail.com>
2025-11-25 18:54:48 -0300

    Atualização 25/11 - 18h54

--- src/data/bolosDecorados.jsx
+++ src/data/bolosDecorados.jsx
@@ -3,7 +3,6 @@ export const bolosTradicionais = [
     {id: 0, nome: "Bentô Cake", fatias: "4 fatias"},
     {id: 1, tamanho: "11cm", fatias: "8 fatias"},
     {id: 2, tamanho: "15cm", fatias: "12 fatias"},
-    {id: 3, tamanho: "17cm", fatias: "20 fatias",},
     {id: 4, tamanho: "20cm", fatias: "25 fatias"},
     {id: 5, tamanho: "25cm", fatias: "30 fatias"},
     {id: 6, tamanho: "30cm", fatias: "50 fatias"},
